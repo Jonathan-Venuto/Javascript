d4aa2113ecdebd852480f2d876a114587ba74add
Jonathan-Venuto <jonathanvenutopro@gmail.com>
2023-01-17 13:41:53 -0300

    ex 11

---
+++ aula11/ex008.js
@@ -0,0 +1,10 @@
+  // CONDIÇÃO SIMPLES (APENAS "IF") //
+
+var vel = 61 // Limite de velocidade 60 km/h
+
+console.log(`A velocidade do seu carro é: ${vel}km/h!`)
+if (vel > 60 ) // Condição Simples. porque?, por que ela não possui "se não! (else)"
+{
+    console.log(`Voce esta acima da velocidade permitida.MULTADO. `)
+    console.log(`Por favor, pare o veículo.`)
+}
---
+++ aula11/ex009.js
@@ -0,0 +1,9 @@
+var país = 'Brasil'
+if(país == 'Brasil')
+{
+    console.log(`Vivendo em ${país}, voce é Brasileiro!`)
+}
+else
+{
+    console.log(`Vivendo em ${país}, voce é Estrangeiro!`)
+}
