61eddf28b2dd7c4c22da9f1e092e7e8a9550fd93
Jonathan-Venuto <jonathanvenutopro@gmail.com>
2023-01-18 09:46:13 -0300

    Update ex011.js

--- aula12/ex011.js
+++ aula12/ex011.js
@@ -1,10 +1,12 @@
 var idade = 20
 
-if(idade < 18)
+if(idade < 16)
 {
     console.log(`Não vota!`)
 }
 else 
 {
-    console.log(`vota!`)
+    if( idade >= 16 && idade <18)
+    { console.log('Voto opcional')}
 }
+
