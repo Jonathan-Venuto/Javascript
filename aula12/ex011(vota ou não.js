f8fd4e2ce3265713f4c4cf741a04a9b90fe5f971
Jonathan-Venuto <jonathanvenutopro@gmail.com>
2023-01-19 11:16:46 -0300

    if, esle , else if

---
+++ aula12/ex011(vota ou não.js
@@ -0,0 +1,20 @@
+var idade = 12
+console.log(`você tem ${idade} anos! `)
+
+
+if(idade < 16)
+{
+ console.log(`Não vota!`)
+}
+else if(idade <18 || idade >=70)
+{
+  {
+    console.log('Voto opcional')
+  }
+}
+else
+{
+    console.log('voto obrigatório!')
+}
+
+
