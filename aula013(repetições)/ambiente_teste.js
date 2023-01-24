eac5a9b8f739ae29056e8f7fcb5e7b235fecf2f3
Jonathan-Venuto <jonathanvenutopro@gmail.com>
2023-01-24 12:49:11 -0300

    repetições

--- aula013(repetições)/ambiente_teste.js
+++ aula013(repetições)/ambiente_teste.js
@@ -1,6 +1,22 @@
-var c = 1
-while( c < 6)
+/*Estrutura de repetição com teste lógico no ---início---*/ 
+
+/*var c = 2 
+while( c <= 6) // testa primeiro
 {
-    console.log(`Ola! tudo bem? passo ${c}`)
+    console.log(`Ola! tudo bem? passo ${c}`) // executa depois!
+    c++
+} 
+
+/* Estrutura de repetição com teste lógico no ---final---*/
+
+
+var c = 1
+do
+{
+    console.log(`Ola! tudo bem? passo ${c}`) // executa primeiro
     c++
 }
+while( c <= 2 ) // testa depois
+
+
+
